0bb4876bd33529d5e208c38db798cf95271c6d19
Nasor2 <penaortegasamuel@gmail.com>
2025-07-19 23:18:29 -0500

    feat: add user DTO Mapper

---
+++ src/main/java/com/nasor/bookingapi/mapper/UserDtoMapper.java
@@ -0,0 +1,15 @@
+package com.nasor.bookingapi.mapper;
+
+import com.nasor.bookingapi.dto.UserDto;
+import com.nasor.bookingapi.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class UserDtoMapper implements Function<User, UserDto> {
+    @Override
+    public UserDto apply(User user) {
+        return new UserDto(user.getId(), user.getFirstName() + " " + user.getLastName(), user.getEmail(), user.getAddress(), user.getPhoneNumber());
+    }
+}
