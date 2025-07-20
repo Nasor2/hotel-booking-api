3bfbe77cfb987e589975874961b11e09cae4012c
Nasor2 <penaortegasamuel@gmail.com>
2025-07-19 23:16:41 -0500

    feat: add repositories

---
+++ src/main/java/com/nasor/bookingapi/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.nasor.bookingapi.repository;
+
+import com.nasor.bookingapi.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
---
+++ src/main/java/com/nasor/bookingapi/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nasor.bookingapi.repository;
+
+import com.nasor.bookingapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
